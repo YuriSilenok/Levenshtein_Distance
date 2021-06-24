6000917756f823f0803d6f51815ad34048eb7811
YuriSilenok <yuri.silenok@gmail.com>
2021-06-24 18:48:22 +0300

    Добавьте файлы проекта.

---
+++ Levenshtein_Distance/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Levenshtein_Distance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(119, 39);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Levenshtein_Distance/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Levenshtein_Distance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length != 0 && textBox2.Text.Length != 0)
+                label1.Text = D(textBox1.Text.Length - 1, textBox2.Text.Length - 1).ToString();
+            else label1.Text = "";
+        }
+
+        private int D(int i, int j)
+        {
+            if (i == 0 && j == 0) return 0;
+            if (j == 0 && i > 0) return i;
+            if (i == 0 && j > 0) return j;
+            if (j > 0 && i > 0)
+            {
+                return min(
+                    D(i, j - 1) + 1,
+                    D(i - 1, j) + 1,
+                    D(i - 1, j - 1) + ((textBox1.Text[i] == textBox2.Text[j]) ? 0 : 1)
+                   );
+            }
+            throw new Exception("Входные параметры не могут быть отрицательными");
+        }
+        private int min(int a, int b, int c)
+        {
+            return (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
+        }
+
+        int[] fibo = new int[] { 1, 2, 3, 5, 8, 13, 21 };
+        
+        private int fiboF(int n)
+        {
+            int first = 1;
+            int second = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int f = first + second;
+                first = second;
+                second = f;
+            }
+            return second;
+        }
+
+        private int indexfOfFib(int[] array, int val, int indStart, int indStop)
+        {
+
+            if (val == array[indStart]) return indStart;
+            if (val == array[indStop]) return indStop;
+            ///если границы стали малы на столько что границы стоят 
+            ///рядом или накладываются друг на друга, 
+            ///то искомого значения нет в масиве
+            if (indStop - indStart < 2) return -1;
+            
+            for (int i = 1; ; i++)
+            {
+                int newIndStart = indStart + fiboF(i - 1);
+                int newIndStop = indStop + fiboF(i);
+                if (newIndStop > indStop) newIndStop = indStop;
+                if (array[newIndStop] == val) return newIndStop;
+                ///нужно взять интервал который ограничен числами фибоначи относительно левой границы массива
+                if (val < array[newIndStop]) return indexfOfFib(array, val, newIndStart, newIndStop);
+                if (newIndStop == indStart && val > array[newIndStop]) return -1;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox3.Text.Length != 0 && textBox4.Text.Length == 1)
+            {
+                List<int> list = new List<int>();
+                foreach (char ch in textBox3.Text)
+                    list.Add((int)ch);
+                int[] array = list.ToArray();
+                int val = textBox4.Text[0];
+                label2.Text = indexfOfFib(array, val, 0, array.Length - 1).ToString();
+            }
+        }
+    }
+}
